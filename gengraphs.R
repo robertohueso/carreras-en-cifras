2cb2c53db45b80297561ebb5b1c781566922fb08
NCordon <nacho.cordon.castillo@gmail.com>
2016-03-05 17:59:53 +0100

    matriculas -> utf8 y gengraphs busca ya asignaturas comunes a ambos datasets

--- gengraphs.R
+++ gengraphs.R
@@ -1,10 +1,42 @@
 # Libreria de graficos
 library(ggplot2)
 
-data <- read.csv2("./listas/INE.csv", header = FALSE, sep=",",dec=".", row.names=c("Carreras",rep(c("Hombres","Mujeres"),2)))
+# Suponemos utf8
+trabajando <- read.csv2(  "./listas/INE.csv", 
+                          col.names=c("Carreras",rep(c("Hombres","Mujeres"),3)),
+                          header = FALSE, 
+                          sep=",",
+                          dec="."
+                        )
+                        
+matriculas <- read.csv2(  "./listas/201415matriculasgradoramas.csv",
+                          col.names = c("Ramas","Carreras","Hombres","Mujeres","Total"), 
+                          header = TRUE, 
+                          sep=","
+                        )
 
 
-prueba <- c("Hombres", "Mujeres")
+# Limpiamos un poco el dataset
+trabajando <- na.omit( trabajando )
 
-prueba**2
-rep(prueba,2)
+
+
+
+# Number of columns of dataset
+n <- ncol(trabajando)
+
+trabajando.titul <- sapply( trabajando$Carreras, tolower )
+matriculas.titul <- sapply( matriculas$Carreras, tolower )
+# Asignaturas comunes a los dos datasets
+comunes <- intersect( trabajando.titul, matriculas.titul)
+
+
+
+trabajando <- trabajando[ order( trabajando$Carreras ), ]
+matriculas <- matriculas[ order( matriculas$Carreras ), ]
+
+
+
+trabajando <- trabajando[,c(1,n-1,n)]
+matriculas<- matriculas[ c("Carreras", "Hombres", "Mujeres") ]
+
